c754c6b13c006213468efdab29fcf01855239b21
EritzSM <esanchez701@soyudemedellin.edu.co>
2026-04-28 18:28:52 -0500

    fix: extract explicit BACKEND_HOST to resolve Render routing no-deploy error

--- cocina-compartida/docker-entrypoint.render.sh
+++ cocina-compartida/docker-entrypoint.render.sh
@@ -5,9 +5,12 @@ set -e
 
 : "${BACKEND_URL:?La variable BACKEND_URL es requerida}"
 
-envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template \
+# Extraer el host (ej. cocina-compartida-api.onrender.com) de la URL
+export BACKEND_HOST=$(echo "$BACKEND_URL" | awk -F/ '{print $3}')
+
+envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
-echo "Nginx configurado con BACKEND_URL=$BACKEND_URL"
+echo "Nginx configurado con BACKEND_URL=$BACKEND_URL y BACKEND_HOST=$BACKEND_HOST"
 
 exec nginx -g 'daemon off;'
