59de5cd08d03c9fa1fbb079675b471b891704b13
EritzSM <esanchez701@soyudemedellin.edu.co>
2026-04-29 08:30:59 -0500

    fix: switch to Render internal networking to permanently resolve 502 Bad Gateway

--- cocina-compartida/docker-entrypoint.render.sh
+++ cocina-compartida/docker-entrypoint.render.sh
@@ -5,12 +5,9 @@ set -e
 
 : "${BACKEND_URL:?La variable BACKEND_URL es requerida}"
 
-# Extraer el host (ej. cocina-compartida-api.onrender.com) de la URL
-export BACKEND_HOST=$(echo "$BACKEND_URL" | awk -F/ '{print $3}')
-
-envsubst '${BACKEND_URL} ${BACKEND_HOST}' < /etc/nginx/conf.d/default.conf.template \
+envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
-echo "Nginx configurado con BACKEND_URL=$BACKEND_URL y BACKEND_HOST=$BACKEND_HOST"
+echo "Nginx configurado con BACKEND_URL=$BACKEND_URL (Internal Networking)"
 
 exec nginx -g 'daemon off;'
