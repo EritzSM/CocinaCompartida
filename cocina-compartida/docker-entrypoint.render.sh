e7271be9ed699b1d469c41dbdcd9e4836be040f9
EritzSM <esanchez701@soyudemedellin.edu.co>
2026-04-29 08:57:53 -0500

    fix: revert internal networking since Render Free Tier blocks it, restore public proxy with SNI

--- cocina-compartida/docker-entrypoint.render.sh
+++ cocina-compartida/docker-entrypoint.render.sh
@@ -5,12 +5,14 @@ set -e
 
 : "${BACKEND_URL:?La variable BACKEND_URL es requerida}"
 
+export BACKEND_HOST=$(echo "$BACKEND_URL" | awk -F/ '{print $3}')
+
 # Extraer el DNS interno del sistema para que Nginx pueda resolver hosts dinámicamente
 export NAMESERVER=$(awk '/^nameserver/ {print $2; exit}' /etc/resolv.conf)
 
-envsubst '${BACKEND_URL} ${NAMESERVER}' < /etc/nginx/conf.d/default.conf.template \
+envsubst '${BACKEND_URL} ${BACKEND_HOST} ${NAMESERVER}' < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
-echo "Nginx configurado con BACKEND_URL=$BACKEND_URL y NAMESERVER=$NAMESERVER"
+echo "Nginx configurado con BACKEND_URL=$BACKEND_URL, BACKEND_HOST=$BACKEND_HOST y NAMESERVER=$NAMESERVER"
 
 exec nginx -g 'daemon off;'
